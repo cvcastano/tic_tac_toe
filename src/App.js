bfa0afe1bdf4258ae8164925c5a8e3f555730ff9
cvcastano <carocastanocastillo@gmail.com>
2021-06-23 17:22:08 +0200

    Fix typo

--- src/App.js
+++ src/App.js
@@ -51,12 +51,12 @@ class App extends React.Component {
           {this.renderCell(0)}
           {this.renderCell(1)}
           {this.renderCell(2)}
+          {this.renderCell(3)}
           {this.renderCell(4)}
           {this.renderCell(5)}
           {this.renderCell(6)}
           {this.renderCell(7)}
           {this.renderCell(8)}
-          {this.renderCell(9)}
         </div>
       </div>
     )
